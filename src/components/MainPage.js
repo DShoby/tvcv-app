3c64cd2254bb8293cdf4ffb1cb73a5c3ca6e5a8e
Emmanuelle CORREIA <emmanuelle.correia@outlook.fr>
2018-05-16 10:30:29 +0200

    ajout du btn emploi store et retouches design

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -6,13 +6,20 @@ class MainPage extends Component {
     render() {
         return (
             <div className="row">
-                <div className="col-xs-10 col-xs-offset-1">
-                    <div className="col-xs-8 col-xs-offset-2">
-                        <button className="btn1 btn btn-lg col-xs-6 col-xs-offset-3">Je n'ai pas de compte, m'inscrire</button>
+                <div className="col-xs-10 col-xs-offset-1 btn-content">
+                    <div className="col-xs-6 btn-grp-1">
+                        <button className="btn0 btn btn-lg col-xs-8 col-xs-offset-2">Accéder directement à l'Emploi Store, sans me connecter</button>
+                        <div className={"tooltip0 col-xs-8 col-xs-offset-2"}>
+                            <h4>J'aimerais trouver la formation et le métier qui me correspondent, créer mon entreprise ou même travailler à l'étranger.</h4>
+                        </div>
+
+                    </div>
+                    <div className="col-xs-6">
+                        <button className="btn1 btn btn-lg col-xs-8 col-xs-offset-2">Je n'ai pas de compte, m'inscrire</button>
                         <Link to={"/gotAccount"}>
-                            <button className="btn2 btn btn-lg col-xs-6 col-xs-offset-3">J'ai déjà un compte, me connecter</button>
+                            <button className="btn2 btn btn-lg col-xs-8 col-xs-offset-2">J'ai déjà un compte, me connecter</button>
                         </Link>
-                        <button className="btn3 btn btn-lg col-xs-6 col-xs-offset-3">J'avais un compte, me réinscrire</button>
+                        <button className="btn3 btn btn-lg col-xs-8 col-xs-offset-2">J'avais un compte, me réinscrire</button>
                     </div>
                 </div>
             </div>
