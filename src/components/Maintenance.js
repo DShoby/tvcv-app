e673f9a3e69a18b4b6332df1e61cac77a56ec4d4
Disa91 <qvias91@hotmail.fr>
2018-05-16 15:53:30 +0200

    maintenance update

--- src/components/Maintenance.js
+++ src/components/Maintenance.js
@@ -1,12 +1,22 @@
 import React, {Component} from 'react';
 import maintenance from '../assets/images/maintenance.png';
+import {Link} from "react-router-dom";
 
 class Maintenance extends Component {
     render() {
         return (
-            <div className="center-block">
-                    <h3 className="text-center"> Maintenance en cours sur la page.</h3>
+            <div className={"content-area"}>
+                <div className={"row"}>
+                    <Link to={'/'} className={"col-xs-1"}>
+                        <button className={"back-btn btn btn-lg col-xs-12"}>Retour</button>
+                    </Link>
+                </div>
+
+
+                <div className="center-block">
+                    <h3 className="text-center"> Maintenance en cours sur la page</h3>
                     <img src={maintenance} alt="maintenance" className="center-block"/>
+                </div>
             </div>
         );
     }
